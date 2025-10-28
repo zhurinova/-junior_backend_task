b946b11304b53f560542cff8590a9d35317069b5
zhurinova <102167193+zhurinova@users.noreply.github.com>
2025-10-28 13:45:43 +0300

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,30 @@
+psql -U "postgres" "junior_backend_task"
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    parent_id INTEGER REFERENCES categories(id) ON DELETE SET NULL
+);
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity INTEGER NOT NULL DEFAULT 0,
+    price DECIMAL(10,2) NOT NULL,
+    category_id INTEGER REFERENCES categories(id)
+);
+CREATE TABLE clients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address TEXT
+);
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER REFERENCES clients(id),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders(id),
+    product_id INTEGER REFERENCES products(id),
+    quantity INTEGER NOT NULL,
+    price DECIMAL(10,2) NOT NULL -- цена на момент заказа
+);
